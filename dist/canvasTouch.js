d9930ddf52b84672b403268ee7c8358db656acd7
botiioi <57485759+botiioi@users.noreply.github.com>
2020-04-14 20:58:55 +0300

    Add files via upload

--- dist/canvasTouch.js
+++ dist/canvasTouch.js
@@ -15,7 +15,7 @@
 
 	
 
-	var maxRadius = 45;
+	var maxRadius = 40;
 	// minRadius = 8;
 
 	var colorArray = [
@@ -68,8 +68,8 @@ window.addEventListener('touchmove', function(e) {
 
 			//interactiviti
 
-			if (touch.x - this.x < 80 && touch.x - this.x > -80 && touch.y - this.y < 80 && touch.y - this.y > -
-				80) { //финт ушами!
+			if (touch.x - this.x < 70 && touch.x - this.x > -70 && touch.y - this.y < 70 && touch.y - this.y > -
+				70) { //финт ушами!
 				if (this.radius < maxRadius) {
 					this.radius += 1;
 				}
